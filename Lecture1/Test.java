9f6072a673459739563c27be078a792bd4719fca
SAKSHAM CHAUHAN <sakshamchauhan@SAKSHAMs-MacBook-Air.local>
2025-04-11 01:27:58 +0530

    LOOPS

---
+++ Lecture1/Test.java
@@ -0,0 +1,10 @@
+package Lecture1;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Welcome to");
+        System.out.print("Coding ");
+        System.out.println("Blocks");
+        System.out.print("Bye Bye");
+    }
+}
