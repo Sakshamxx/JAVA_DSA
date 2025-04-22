f0016ccccb99059dff7313f69bdf9275d8cf20ca
SAKSHAM CHAUHAN <sakshamchauhan@SAKSHAMs-MacBook-Air.local>
2025-04-23 04:26:31 +0530

    SESSION-01

--- Lecture1/Home_Work.java
+++ Lecture1/Home_Work.java
@@ -2,22 +2,22 @@ package Lecture1;
 
 public class Home_Work {
     public static void main(String[] args) {
-        int n=100;
-        int i=1;
-        while(i<=n){
-            if(i%3==0){
-                i=i+1;
+        int n = 100;
+        int i = 1;
+        while (i <= n) {
+            if (i % 3 == 0) {
+                i = i + 1;
                 continue;
             }
             System.out.println(i);
-            i=i+1;
+            i = i + 1;
         }
 
         // for(int i=1;i<=n;i++){
-        //     if(i%3==0){
-        //         continue;
-        //     }
-        //     System.out.println(i+" ");
+        // if(i%3==0){
+        // continue;
+        // }
+        // System.out.println(i+" ");
         // }
     }
 }
