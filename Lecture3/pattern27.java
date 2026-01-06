e0b69d35f3751b8b8f5148ea5026c9dd0463a2ed
SAKSHAM CHAUHAN <sakshamchauhan@SAKSHAMs-MacBook.local>
2026-01-06 23:41:51 +0530

    Lecture-3

---
+++ Lecture3/pattern27.java
@@ -0,0 +1,37 @@
+package Lecture3;
+
+import java.util.Scanner;
+
+public class pattern27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int star = 1;
+        int space = n - 1;
+        int row = 1;
+        while (row <= n) {
+            int i = 1;
+            while (i <= space) {
+                System.out.print("  ");
+                i++;
+            }
+            int val=1;
+            int j = 1;
+            while (j <= star) {
+                System.out.print(val+" ");
+                if (j<=star/2){
+                    val++;
+                }else{
+                    val--;
+                }
+                j++;
+            }
+
+            row++;
+            star += 2;
+            space--;
+            System.out.println();
+        }
+        sc.close();
+    }
+}
---
+++ Lecture3/patternRhombus.java
@@ -0,0 +1,39 @@
+package Lecture3;
+import java.util.Scanner;
+public class patternRhombus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int space=n-1/2;
+        int star=1;
+        int row=1;
+        int val=1;
+        while(row<=n){
+            int i=1;
+            while(i<=space){
+                System.out.print("  ");
+                i++;
+            }
+            int j=1;
+            while(j<=star){
+                System.out.print(val+" ");
+                if (j<=n/2){
+                    val++;
+                }else{
+                    val--;
+                }
+                j++;
+            }
+            if (row<=n/2){
+                space--;
+                star+=2;
+            }else{
+                space++;
+                star-=2;
+            }
+            row++;
+            System.out.println();
+        }
+        sc.close();
+    }
+}
